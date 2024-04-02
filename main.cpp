0bd4c52229d86bd3ea8d954d1eb80b00938a324e
omarandlorraine <sam.magnus.wilson@gmail.com>
2024-04-02 12:16:00 +0100

    add examples in main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "ssdp.hpp"
+
+void print_them_out(std::string result) {
+	std::cout << result << std::endl;
+}
+
+auto main(int argc, char* argv[]) -> int {
+	for (int i = 1; i < argc; ++i) {
+		if(!strcmp(argv[i], "--uuid")) {
+			ssdp::discover(ssdp::search_target_uuid(argv[++i]), print_them_out);
+		} else if(!strcmp(argv[i], "--all")) {
+			ssdp::discover(ssdp::search_target_all(), print_them_out);
+		} else {
+			std::cerr << "unknown command-line argument '" << argv[i] << "'" << std::endl;
+		}
+		i++;
+	}
+
+	return 0;
+}
